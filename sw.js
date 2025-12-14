4d212ed2763396247fca58c53ead2e1a2e361cb1
Rokon Uz Zaman Remon <128873194+rokonuzzamanremon03@users.noreply.github.com>
2025-12-14 13:32:54 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mouza-app-v3-glassy'; // ভার্সন নাম পরিবর্তন করা হলো
+const CACHE_NAME = 'mouza-app-v4-green'; // ভার্সন নাম পরিবর্তন
 const urlsToCache = [
   './',
   './index.html',
@@ -7,9 +7,9 @@ const urlsToCache = [
   './images/logo2.png'
 ];
 
-// Install Event
+// Install
 self.addEventListener('install', (event) => {
-  self.skipWaiting(); // নতুন সার্ভিস ওয়ার্কারকে সাথে সাথে একটিভ হতে বাধ্য করা
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
@@ -17,7 +17,7 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Activate Event (পুরানো ক্যাশ ডিলিট করার জন্য)
+// Activate (Old cache clear)
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -34,11 +34,10 @@ self.addEventListener('activate', (event) => {
   return self.clients.claim();
 });
 
-// Fetch Event
+// Fetch
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // ক্যাশ থেকে পেলে ভালো, না হলে নেটওয়ার্ক থেকে আনবে
       return response || fetch(event.request).then((networkResponse) => {
           return caches.open(CACHE_NAME).then((cache) => {
               cache.put(event.request, networkResponse.clone());
